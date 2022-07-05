828fd5248e104dee162a02c885b4f57ac15cf44c
yanethvg <zoilavillatoro6694@gmail.com>
2022-07-05 09:08:48 -0600

    agregada clase de Operadores aritméticos con PHP

---
+++ operadores-aritmeticos.php
@@ -0,0 +1,22 @@
+<?php
+$ValA = 5;
+$ValB =2;
+
+$suma =$ValA + $ValB;
+var_dump($suma);
+
+$resta =$ValA - $ValB;
+var_dump($resta);
+
+$multiplicacion =$ValA * $ValB;
+var_dump($multiplicacion);
+
+$division =$ValA / $ValB;
+var_dump($division);
+
+$modulo =$ValA % $ValB;
+var_dump($modulo);
+
+$potencia =$ValA ** $ValB;
+var_dump($potencia);
+?> 
