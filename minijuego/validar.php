b506e552715894da4a4b0d79b21c19adad717de6
yanethvg <zoilavillatoro6694@gmail.com>
2022-07-11 10:48:36 -0600

    Finalizando nuestro juego

--- minijuego/validar.php
+++ minijuego/validar.php
@@ -6,11 +6,11 @@
 
     for ($i=0; $i < count($palabras); $i++) { 
         if (strtolower($_REQUEST["valorPosicion".$i]) == strtolower( $palabras[$i])) {
-            echo "La Palabra ingresada es correcta $i \n";
+            echo "La Palabra ingresada es correcta $i, su respuesta fue ".$_REQUEST["valorPosicion".$i];
         }else{
-            echo "Lo siento el valor colocado es incorrecta, la respuesta correcta es: $palabras[$i] \n";
+            echo "Lo siento el valor colocado es incorrecta, la respuesta correcta es: $palabras[$i] ";
         }
-        echo "\n";
+        echo "<br>";
     }
 
 ?>
