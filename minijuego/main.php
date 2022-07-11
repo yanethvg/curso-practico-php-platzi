3bff630cd3b051d6b8fcef33b427930354e219b6
yanethvg <zoilavillatoro6694@gmail.com>
2022-07-11 10:45:39 -0600

    Finalizando nuestro juego

--- minijuego/main.php
+++ minijuego/main.php
@@ -8,13 +8,13 @@ $cajaDeTexto = "";
 //Desordenar las palabras
 for ($i=0; $i < count($palabras); $i++) { 
     $palabrasDesordenadas[$i] = str_shuffle($palabras[$i]);
-    $cajaDeTexto .=  "<label>Escribe el valor de la posicion ".$i."</label>
+    $cajaDeTexto .=  "<label>Escribe la palabra correcta de  ".$palabrasDesordenadas[$i]." esta posición es $i</label>
     <input type='text' name='valorPosicion".$i."'><br>";
 }
 $botonEnviar = "<button type='submit'>Enviar</button>";
 $finalFormulario = "</form>";
 
-print_r($palabrasDesordenadas);
+// print_r($palabrasDesordenadas);
 echo $formulario.$cajaDeTexto.$botonEnviar.$finalFormulario;
 
 
